a5a412c12f228d9aadedddca73aee9e692287b31
LainonShiraya <konradmmo@gmail.com>
2020-10-15 23:43:56 +0200

    Added animations
    I had to redo the files changing some, so thought it would be easier to update update them that way

--- relative website project/scripts.js
+++ relative website project/scripts.js
@@ -1,12 +1,31 @@
-//Select element function
+// Scroll reveal
 
-const selectElement = function (element) {
-  return document.querySelector(element);
-};
+window.sr = ScrollReveal();
 
-let menuToggler = selectElement(".menu-toggle");
-let body = selectElement("body");
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 200,
+});
 
-menuToggler.addEventListener("click", function () {
-  body.classList.toggle("open");
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 400,
+});
+
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 400,
+});
+
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 400,
 });
